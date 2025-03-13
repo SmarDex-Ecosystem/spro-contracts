ea93adf660e9b7a0222cfc9714154b6735f4febc
lilyanb <lilyan.bastien@gmail.com>
2025-03-13 10:10:57 +0100

    feat: remove unnecessary returns variables

--- src/spro/Spro.sol
+++ src/spro/Spro.sol
@@ -113,20 +113,24 @@ contract Spro is SproStorage, ISpro, Ownable2Step, ReentrancyGuard {
     }
 
     /// @inheritdoc ISpro
-    function createProposal(Proposal calldata proposal, bytes calldata permit2Data) external nonReentrant {
-        (address collateral, uint256 collateralAmount) = _makeProposal(proposal);
+    function createProposal(Proposal memory proposal, bytes calldata permit2Data) external nonReentrant {
+        _makeProposal(proposal);
 
         // Execute permit2Data for the caller
         if (permit2Data.length > 0) {
             (IAllowanceTransfer.PermitBatch memory permitBatch, bytes memory data) =
                 abi.decode(permit2Data, (IAllowanceTransfer.PermitBatch, bytes));
             PERMIT2.permit(msg.sender, permitBatch, data);
-            PERMIT2.transferFrom(msg.sender, address(this), collateralAmount.toUint160(), collateral);
+            PERMIT2.transferFrom(
+                msg.sender, address(this), proposal.collateralAmount.toUint160(), proposal.collateralAddress
+            );
             if (_fee > 0) {
                 PERMIT2.transferFrom(msg.sender, DEAD_ADDRESS, _fee.toUint160(), address(SDEX));
             }
         } else {
-            IERC20Metadata(collateral).safeTransferFrom(msg.sender, address(this), collateralAmount);
+            IERC20Metadata(proposal.collateralAddress).safeTransferFrom(
+                msg.sender, address(this), proposal.collateralAmount
+            );
             if (_fee > 0) {
                 IERC20Metadata(SDEX).safeTransferFrom(msg.sender, DEAD_ADDRESS, _fee);
             }
@@ -331,13 +335,8 @@ contract Spro is SproStorage, ISpro, Ownable2Step, ReentrancyGuard {
     /**
      * @notice Make a proposal.
      * @param proposal The proposal structure.
-     * @return collateral_ The address of the collateral token.
-     * @return collateralAmount_ The amount of collateral.
      */
-    function _makeProposal(Proposal memory proposal)
-        internal
-        returns (address collateral_, uint256 collateralAmount_)
-    {
+    function _makeProposal(Proposal memory proposal) internal {
         if (proposal.startTimestamp >= proposal.loanExpiration) {
             revert InvalidDurationStartTime();
         }
@@ -361,9 +360,7 @@ contract Spro is SproStorage, ISpro, Ownable2Step, ReentrancyGuard {
         }
 
         _proposalsMade[proposalHash] = true;
-        collateral_ = proposal.collateralAddress;
-        collateralAmount_ = proposal.collateralAmount;
-        _withdrawableCollateral[proposalHash] = collateralAmount_;
+        _withdrawableCollateral[proposalHash] = proposal.collateralAmount;
 
         emit ProposalCreated(proposalHash, proposal.proposer, proposal);
     }
