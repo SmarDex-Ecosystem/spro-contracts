29ea0a0e6e2eb0d7c7d78bbc8c22aea72c2a54f6
Lilyan Bastien <91454320+lilyanB@users.noreply.github.com>
2025-03-25 11:26:06 +0100

    test: adding tests and refactor test architecture (#50)
    * test: adding tests
    
    * test: _getLoanStatus tested
    
    * test: simplify fixture
    
    * test: fix fork fixtures
    
    * test: rename tests and remove comments
    
    * fix: fix since merge
    
    * chore: merge branch 'main' into test/adding-tests
    
    * test: fix some tests
    
    * test: fix another test
    
    * test: reorder tests
    
    * test: remove unnecessary struct and change naming
    
    * test: follow recommendation
    
    * test: remove unnecessary setter
    
    * test: fix since merge main
    
    * test: rename t20
    
    * test: follow recommendations
    
    * test: simplify tests
    
    * test: follow recommendation
    
    * test: remove unnecessary test
    
    * test: follow recommendation
    
    * test: calc value

---
+++ test/integration/SproIntegrationClaimLoan.t.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.0;
+
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import { SDBaseIntegrationTest } from "test/integration/utils/Fixtures.sol";
+
+import { ISproErrors } from "src/interfaces/ISproErrors.sol";
+
+contract SproIntegrationClaimLoan is SDBaseIntegrationTest {
+    function setUp() public {
+        _setUp(false);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  claimLoan                                 */
+    /* -------------------------------------------------------------------------- */
+
+    function test_RevertWhen_claimLoanCallerNotLoanTokenHolder() external {
+        _createERC20Proposal();
+        uint256 loanId = _createLoan(proposal, CREDIT_AMOUNT, "");
+
+        vm.startPrank(borrower);
+        // Borrower approvals for credit token
+        credit.mint(borrower, FIXED_INTEREST_AMOUNT); // helper step: mint fixed interest amount for the borrower
+        credit.approve(address(spro), CREDIT_AMOUNT + FIXED_INTEREST_AMOUNT);
+        vm.stopPrank();
+
+        // Transfer loanToken to this address
+        vm.prank(lender);
+        loanToken.transferFrom(lender, address(this), loanId);
+
+        // Initial lender repays loan
+        vm.startPrank(lender);
+        vm.expectRevert(ISproErrors.CallerNotLoanTokenHolder.selector);
+        spro.claimLoan(loanId);
+    }
+
+    function test_RevertWhen_claimLoanRunningAndExpired() external {
+        _createERC20Proposal();
+        uint256 loanId = _createLoan(proposal, CREDIT_LIMIT, "");
+
+        // Borrower approvals for credit token
+        vm.startPrank(borrower);
+        credit.mint(borrower, FIXED_INTEREST_AMOUNT); // helper step: mint fixed interest amount for the borrower
+        credit.approve(address(spro), CREDIT_LIMIT + FIXED_INTEREST_AMOUNT);
+        vm.stopPrank();
+
+        // Transfer loanToken to this address
+        vm.prank(lender);
+        loanToken.transferFrom(lender, address(this), loanId);
+
+        skip(100 days); // loan should be expired
+        require(spro.getLoan(loanId).loanExpiration <= block.timestamp, "Loan should be expired");
+
+        // loan token holder claims the expired loan
+        spro.claimLoan(loanId);
+
+        assertEq(collateral.balanceOf(address(this)), proposal.collateralAmount); // collateral amount transferred to
+            // loan token holder
+        assertEq(loanToken.balanceOf(address(this)), 0); // loanToken balance should be zero now
+    }
+
+    function test_RevertWhen_claimLoan_LoanRunning() external {
+        _createERC20Proposal();
+        uint256 loanId = _createLoan(proposal, CREDIT_AMOUNT, "");
+
+        vm.startPrank(borrower);
+        // Borrower approvals for credit token
+        credit.mint(borrower, FIXED_INTEREST_AMOUNT); // helper step: mint fixed interest amount for the borrower
+        credit.approve(address(spro), CREDIT_AMOUNT + FIXED_INTEREST_AMOUNT);
+        vm.stopPrank();
+
+        // Try to repay loan
+        vm.startPrank(lender);
+        vm.expectRevert(ISproErrors.LoanRunning.selector);
+        spro.claimLoan(loanId);
+    }
+}
--- test/integration/utils/Fixtures.sol
+++ test/integration/utils/Fixtures.sol
@@ -2,55 +2,116 @@
 pragma solidity >=0.8.0;
 
 import { Test } from "forge-std/Test.sol";
-import { T20 } from "test/helper/T20.sol";
 
 import { IAllowanceTransfer } from "permit2/src/interfaces/IAllowanceTransfer.sol";
-import { PermitSignature } from "permit2/test/utils/PermitSignature.sol";
 
+import { T20 } from "test/helper/T20.sol";
+
+import { ISproTypes } from "src/interfaces/ISproTypes.sol";
 import { Spro } from "src/spro/Spro.sol";
 import { SproLoan } from "src/spro/SproLoan.sol";
 
-abstract contract SproForkBase is Test, PermitSignature {
-    address payable constant PERMIT = payable(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
+contract SDBaseIntegrationTest is Test {
+    T20 collateral;
+    T20 credit;
+
+    address lender = vm.addr(777);
+    address borrower = vm.addr(888);
+    ISproTypes.Proposal proposal;
+
+    // Additional lenders
+    address alice;
+    uint256 aliceKey;
+    address bob;
+    address charlie;
+
+    // Constants
+    uint256 public constant COLLATERAL_AMOUNT = 10_000e18;
+    uint256 public constant INITIAL_CREDIT_BALANCE = 1000e18;
+    uint256 public constant CREDIT_AMOUNT = 60e18;
+    uint256 public constant CREDIT_LIMIT = 100e18;
+    uint256 public constant FIXED_INTEREST_AMOUNT = 5e18;
+    uint256 public constant INITIAL_SDEX_BALANCE = 1_000_000e18;
+    address payable constant PERMIT2 = payable(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
+    address public constant ADMIN = address(0x1212121212121212121212121212121212121212);
     uint256 public constant FEE = 20e18;
     uint16 public constant PARTIAL_POSITION_BPS = 500;
 
-    Deployment deployment;
+    Spro spro;
+    SproLoan loanToken;
+    T20 sdex;
+    IAllowanceTransfer permit2;
 
-    // Properties need to be in alphabetical order
-    struct Deployment {
-        Spro config;
-        SproLoan loanToken;
-        address proxyAdmin;
-        address protocolAdmin;
-        T20 sdex;
-        IAllowanceTransfer permit2;
+    function _setUp(bool fork) public virtual {
+        if (fork) {
+            string memory url = vm.rpcUrl("mainnet");
+            vm.createSelectFork(url);
+            permit2 = IAllowanceTransfer(PERMIT2);
+        } else {
+            permit2 = IAllowanceTransfer(makeAddr("IAllowanceTransfer"));
+        }
+        sdex = new T20();
+
+        vm.prank(ADMIN);
+        spro = new Spro(address(sdex), address(permit2), FEE, PARTIAL_POSITION_BPS);
+
+        loanToken = spro._loanToken();
+
+        // Deploy tokens
+        collateral = new T20();
+        credit = new T20();
+
+        proposal = ISproTypes.Proposal(
+            address(collateral),
+            COLLATERAL_AMOUNT,
+            address(credit),
+            CREDIT_LIMIT,
+            FIXED_INTEREST_AMOUNT,
+            uint40(block.timestamp) + 5 days,
+            uint40(block.timestamp) + 10 days,
+            borrower,
+            0,
+            PARTIAL_POSITION_BPS
+        );
+
+        // Mint and approve SDEX
+        sdex.mint(lender, INITIAL_SDEX_BALANCE);
+        vm.prank(lender);
+        sdex.approve(address(spro), type(uint256).max);
+        sdex.mint(borrower, INITIAL_SDEX_BALANCE);
+        vm.prank(borrower);
+        sdex.approve(address(spro), type(uint256).max);
+
+        // Setup lender addresses
+        (alice, aliceKey) = makeAddrAndKey("alice");
+        bob = makeAddr("bob");
+        charlie = makeAddr("charlie");
     }
 
-    function setUp() public virtual {
-        string memory url = vm.rpcUrl("mainnet");
-        vm.createSelectFork(url);
-        deployment.proxyAdmin = makeAddr("proxyAdmin");
-        deployment.protocolAdmin = address(this);
+    function _createERC20Proposal() internal {
+        collateral.mint(borrower, proposal.collateralAmount);
 
-        // Deploy SDEX token
-        deployment.sdex = new T20();
+        vm.prank(borrower);
+        collateral.approve(address(spro), proposal.collateralAmount);
+        vm.prank(borrower);
+        spro.createProposal(proposal, "");
+    }
 
-        deployment.permit2 = IAllowanceTransfer(PERMIT);
+    function _createLoan(ISproTypes.Proposal memory newProposal, uint256 amount, bytes memory revertData)
+        internal
+        returns (uint256 loanId)
+    {
+        // Mint initial state & approve credit
+        credit.mint(lender, INITIAL_CREDIT_BALANCE);
+        vm.prank(lender);
+        credit.approve(address(spro), CREDIT_LIMIT);
 
-        vm.startPrank(deployment.protocolAdmin);
+        // Create Loan
+        if (keccak256(revertData) != keccak256("")) {
+            vm.expectRevert(revertData);
+        }
 
-        // Deploy protocol
-        deployment.config = new Spro(address(deployment.sdex), address(deployment.permit2), FEE, PARTIAL_POSITION_BPS);
-        vm.stopPrank();
-        deployment.loanToken = deployment.config._loanToken();
-
-        // Labels
-        vm.label(deployment.proxyAdmin, "proxyAdmin");
-        vm.label(deployment.protocolAdmin, "protocolAdmin");
-        vm.label(address(deployment.sdex), "sdex");
-        vm.label(address(deployment.permit2), "permit2");
-        vm.label(address(deployment.config), "config");
-        vm.label(address(deployment.loanToken), "loanToken");
+        vm.prank(lender);
+        return spro.createLoan(newProposal, amount, "");
     }
 }
