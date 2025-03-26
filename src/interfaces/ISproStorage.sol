14e5f4634a8c696096596959bda3a43af3dd8f81
samooyo <samidarnaud@gmail.com>
2025-03-26 16:00:21 +0100

    style: add end underscore to returned variable

--- src/interfaces/ISproStorage.sol
+++ src/interfaces/ISproStorage.sol
@@ -38,9 +38,9 @@ interface ISproStorage {
 
     /**
      * @notice Retrieves the current proposal nonce.
-     * @return proposalNonce The current proposal nonce.
+     * @return _proposalNonce The current proposal nonce.
      */
-    function _proposalNonce() external view returns (uint256 proposalNonce);
+    function _proposalNonce() external view returns (uint256 _proposalNonce);
 
     /**
      * @notice Retrieves the minimum usage ratio for partial lending, expressed in basis points.
