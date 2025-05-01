35d757ad51d1f799d7a34e554a0c33e2b3b9950c
lilyanb <lilyan.bastien@gmail.com>
2025-05-01 18:17:48 +0200

    test: adding ENDLOAN_01

--- test/fuzzing/conditions/PostconditionsSpro.sol
+++ test/fuzzing/conditions/PostconditionsSpro.sol
@@ -87,7 +87,7 @@ contract PostconditionsSpro is Properties {
             invariant_REPAY_02(loanWithId);
             invariant_REPAY_03(loanWithId, actors[2]);
             invariant_REPAY_04(loanWithId, actors[1], actors[0]);
-            invariant_ENDLOAN_01(actors[0]);
+            invariant_ENDLOAN_01(actors[0], loanWithId.loanId);
             invariant_ENDLOAN_02(actors[1], actors[0], loanWithId.loanId);
             invariant_ENDLOAN_03(loanWithId.loanId);
             invariant_ENDLOAN_04(loanWithId, actors[0]);
@@ -110,7 +110,7 @@ contract PostconditionsSpro is Properties {
             invariant_CLAIM_01(loanWithId.loanId);
             invariant_CLAIM_02(loanWithId);
             invariant_CLAIM_03(loanWithId, actors[0]);
-            // invariant_ENDLOAN_01(actors[0]);
+            invariant_ENDLOAN_01(actors[0], loanWithId.loanId);
             // invariant_ENDLOAN_02(actors[0], actors[0], loanWithId.loanId);
             // invariant_ENDLOAN_03(loanWithId.loanId);
             // invariant_ENDLOAN_04(loanWithId, actors[0]);
